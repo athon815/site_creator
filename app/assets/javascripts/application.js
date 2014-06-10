9aa3395d439d432dc5f10d05c9a1a68f4d17ff34
Aaron Thon <aaroncthon4815@gmail.com>
2014-06-10 09:46:37 -0500

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,43 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require turbolinks
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
+
+    var key = "draft",
+        contentEl = document.getElementById("content");
+
+    $(document).ready(function() {
+        function checkPosition() {
+            if ($(this).scrollTop() > 200) {
+                $('.go-top').fadeIn(500);
+            } else {
+                $('.go-top').fadeOut(300);
+            }
+        }
+        // Show or hide the sticky footer button
+        $(window).scroll(checkPosition);
+
+        // Animate the scroll to top
+        $('.go-top').click(function(event) {
+            event.preventDefault();
+
+            $('html, body').animate({scrollTop: 100}, 300);
+        })
+
+        checkPosition();
+    }); //btw i dont really know what some of this means. go over it when meet with chris or ryan!!
---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,79 @@
+class SectionsController < ApplicationController
+  before_action :set_section, only: [:show, :edit, :update, :destroy]
+
+  # GET /sections
+  # GET /sections.json
+  def index
+    @page = Page.find(params[:id])
+    @page.sections = Section.all
+  end
+
+  # GET /sections/1
+  # GET /sections/1.json
+  def show
+    @page = Page.find(params[:id])
+    @page.sections = Section.find(params[:id])
+  end
+
+  # GET /sections/new
+  def new
+    @page = Page.find(params[:id])
+    @page.sections = Section.new(:page_id => params[:page_id])
+  end
+
+  # GET /sections/1/edit
+  def edit
+
+  end
+
+  # POST /sections
+  # POST /sections.json
+  def create
+    @page.sections = Section.new(section_params)
+
+    respond_to do |format|
+      if @section.save
+        format.html { redirect_to @page, notice: 'Section was successfully created.' }
+        format.json { render :show, status: :created, location: @section }
+      else
+        format.html { render :new }
+        format.json { render json: @page.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /sections/1
+  # PATCH/PUT /sections/1.json
+  def update
+    respond_to do |format|
+      if @page.sections.update(section_params)
+        format.html { redirect_to @page.sections, notice: 'Section was successfully updated.' }
+        format.json { render :show, status: :ok, location: @section }
+      else
+        format.html { render :edit }
+        format.json { render json: @page.sections.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sections/1
+  # DELETE /sections/1.json
+  def destroy
+    @page.sections.destroy
+    respond_to do |format|
+      format.html { redirect_to page_sections_url, notice: 'Section was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_section
+      @section = @page.sections.new
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def section_params
+      params.require(:section).permit(:title, :body, :page_id)
+    end
+end
