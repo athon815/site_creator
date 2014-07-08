7bc3562f3d59f1ad9a67dde12f757610f22619b2
Aaron Thon <aaroncthon4815@gmail.com>
2014-07-08 08:47:51 -0500

    index on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+  	@pages = Page.all.order(:title)
   end
+
+	private
+
+  def set_page
+    @page = Page.find(params[:id])
+  end
+
+
+	def page_params
+		params.require(:page).permit(:title, :image) if params[:page]
+	end
 end
